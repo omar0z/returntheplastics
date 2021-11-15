e657ffd333c8c4386877d0c856c35fab68cc5a0e
Omar Perez <ometh2@gmail.com>
2021-11-15 17:02:12 +0100

    Adding logging to mailing routine

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,19 @@ Route::get('/', function () {
 
 Route::post('contact', function (Request $request) {
 
-    Mail::to(['rik.stallaerts@gmail.com', 'returntheplastics@gmail.com'])->send(new ContactMail(
-        $request->subject,
-        $request->content,
-        $request->name,
-        $request->email
-    ));
-    return redirect()->back()->with('success', 'Your message has been sent. Thank you!');
+    try {
+        Log::info("Sending email to the admin contacts...");
+
+        Mail::to(['rik.stallaerts@gmail.com', 'returntheplastics@gmail.com'])->send(new ContactMail(
+            $request->subject,
+            $request->content,
+            $request->name,
+            $request->email
+        ));
+
+        Log::info("Mail sent successfully!");
+        return redirect()->back()->with('success', 'Your message has been sent. Thank you!');
+    } catch (Exception $e) {
+        Log::error("Email couldn't be sent. Reason; " . $e->getMessage());
+    }
 })->name('contact');
