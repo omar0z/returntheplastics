4f1f06bb1f39132713f652edd910a0f63f3a006d
Omar Perez <ometh2@gmail.com>
2021-12-21 20:30:01 +0100

    added support for interactive map of the signatures

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,9 +103,9 @@
     <!-- End Google Tag Manager (noscript) -->
 
     @if (session('status'))
-        <div class="alert alert-success">
-            {{ session('status') }}
-        </div>
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
     @endif
 
     <!-- ======= Mobile nav toggle button ======= -->
@@ -133,7 +133,8 @@
         <div class="container" data-aos="zoom-in" data-aos-delay="100">
             <h1>THIS PROJECT IS <span class="typed" style="color: cornflowerblue"
                     data-typed-items="RUBBISH!, CRAP!, A WASTE!"></span></h1>
-            {{-- <p style="font-family: Arvo">Return <span class="typed" data-typed-items="the Plastics, the Ocean, the Purpose"></span></p> --}}
+            {{-- <p style="font-family: Arvo">Return <span class="typed"
+                    data-typed-items="the Plastics, the Ocean, the Purpose"></span></p> --}}
             <p style="font-family: Arvo">Return the Plastics</p>
             <div class="social-links">
                 <a href="https://twitter.com/ReturnPlastics" class="twitter"><i class="bx bxl-twitter"></i></a>
@@ -146,8 +147,8 @@
             </div>
             <br>
             <div class="">
-                <a href="https://eci.ec.europa.eu/020/public/#/screen/home"
-                target="blank" class="btn btn-primary">Support our project here!</a>
+                <a href="https://eci.ec.europa.eu/020/public/#/screen/home" target="blank"
+                    class="btn btn-primary">Support our project here!</a>
             </div>
         </div>
     </section><!-- End Hero -->
@@ -410,198 +411,38 @@
                         </div>
 
                     </div>
-                    <div class="col-lg-3"><a href="https://eci.ec.europa.eu/020/public/#/screen/home"
-                            target="blank" class="btn btn-primary">Sign here!</a></div>
+                    <div class="col-lg-3"><a href="https://eci.ec.europa.eu/020/public/#/screen/home" target="blank"
+                            class="btn btn-primary">Sign here!</a></div>
 
                 </div>
 
             </div>
         </section><!-- End Skills Section -->
 
-        {{-- <!-- ======= Portfolio Section ======= -->
-        <section id="portfolio" class="portfolio section-bg">
+        <!-- ======= Map Section ======= -->
+        <section id="map" class="section-bg">
+
             <div class="container" data-aos="fade-up">
 
                 <div class="section-title">
-                    <h2>Portfolio</h2>
-                    <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
-                        sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias
-                        ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                    <h2>Your support on the map</h2>
+                    <p>This is an interactive map. Feel free to explore it!</p>
                 </div>
 
                 <div class="row">
                     <div class="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-                        <ul id="portfolio-flters">
-                            <li data-filter="*" class="filter-active">All</li>
-                            <li data-filter=".filter-app">App</li>
-                            <li data-filter=".filter-card">Card</li>
-                            <li data-filter=".filter-web">Web</li>
-                        </ul>
-                    </div>
-                </div>
+                        <iframe title="Web map" width="100%" height="1024px"
+                            src="https://app.powerbi.com/view?r=eyJrIjoiMTc2YzQxZjctOTE3MS00MGJhLTkyYzgtMGUwNTY2MTRkNGYxIiwidCI6ImJlNDY1NWRmLWFjNzMtNDAxZi1hN2FlLTE5OGMzYjcyZDBjNiIsImMiOjh9"
+                            frameborder="0" allowFullScreen="true"></iframe>
 
-                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 1</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-1.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="App 1"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-2.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 2</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-3.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="App 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Card 2</h4>
-                                <p>Card</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-4.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Card 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 2</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-5.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Web 2"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>App 3</h4>
-                                <p>App</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-6.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="App 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Card 1</h4>
-                                <p>Card</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-7.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Card 1"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Card 3</h4>
-                                <p>Card</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-8.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Card 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                        <div class="portfolio-wrap">
-                            <img src="images/portfolio/portfolio-9.jpg" class="img-fluid" alt="">
-                            <div class="portfolio-info">
-                                <h4>Web 3</h4>
-                                <p>Web</p>
-                                <div class="portfolio-links">
-                                    <a href="images/portfolio/portfolio-9.jpg" data-gall="portfolioGallery"
-                                        class="venobox" title="Web 3"><i class="bx bx-plus"></i></a>
-                                    <a href="portfolio-details.html" data-gall="portfolioDetailsGallery"
-                                        data-vbtype="iframe" class="venobox" title="Portfolio Details"><i
-                                            class="bx bx-link"></i></a>
-                                </div>
-                            </div>
-                        </div>
                     </div>
 
                 </div>
-
-            </div>
-        </section><!-- End Portfolio Section --> --}}
+        </section><!-- Map Portfolio Section -->
 
 
-        {{-- <!-- ======= Testimonials Section ======= -->
+        {{--
+        <!-- ======= Testimonials Section ======= -->
         <section id="testimonials" class="testimonials section-bg">
             <div class="container" data-aos="fade-up">
 
@@ -784,7 +625,9 @@
 
     <!-- <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a> --}}-->
     <div class="row">
-        <a href="https://eci.ec.europa.eu/020/public/#/screen/home" target="_blank" class="back-to-top col-xscol-3  btn btn-primary"> <i class='bx bxs-like bx-tada-hover bx-edit-alt'></i> <span>Support our project</span></a>
+        <a href="https://eci.ec.europa.eu/020/public/#/screen/home" target="_blank"
+            class="back-to-top col-xscol-3  btn btn-primary"> <i class='bx bxs-like bx-tada-hover bx-edit-alt'></i>
+            <span>Support our project</span></a>
     </div>
     <div id="preloader"></div>
 
